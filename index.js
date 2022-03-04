272eba5fad60b4f5d8cb776bfbdc91c2fb9e04c9
timble <timblemichael20@gmail.com>
2022-03-05 09:50:48 +1000

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400){
+    return "This one is on me!"
+  }else if (feet > 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }else if (feet > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  if (city != "NYC") {
+    return "No go."
+  } else {
+    return "Ok, sounds good."
+  }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return "Thank you so much."
+  } else if (tip === 'not as generous') {
+    return "Thank you."
+  } else {
+    return 'Bye.'
+  }
 }
